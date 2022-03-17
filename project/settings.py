2eda85064aff7081bee1b10430369fe74524ca2e
HamdiAAA <hamdi_mohamed2018@hotmail.com>
2022-03-17 14:34:16 -0400

    hot fix -_-

--- project/settings.py
+++ project/settings.py
@@ -190,4 +190,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "hmdmohamed123@gmail.com"
-EMAIL_HOST_PASSWORD = "real2018"
+EMAIL_HOST_PASSWORD = "zjvkflnhmqlykugy"
