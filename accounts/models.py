72fc3ef6c24c199755c1bf45de347c3bae718e5b
HamdiAAA <hamdi_mohamed2018@hotmail.com>
2022-04-20 15:29:31 -0400

    added the verfied field for profiles

--- accounts/models.py
+++ accounts/models.py
@@ -9,9 +9,12 @@ from recipes.models import Recipe
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE , unique=True)
     
+    verified = models.BooleanField(default=True)#setting default as true just for test purposes
+
     #image as a string
     avatar = models.CharField(max_length=8000 , null=True , blank=True , default="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png" )
     bg_image = models.CharField(max_length=8000 , null=True , blank=True , default="https://webgrowhub.com/wp-content/uploads/2020/12/photo-1552566626-52f8b828add9.jpg")
+
 
     #recipes the recipes will be linked by the forgien key to the profile
     @property
